9ac3dcd40ab318c703bb0fcc3589cdd040cb0dab
Isabela Resende <isabelares13@gmail.com>
2023-04-16 21:43:54 -0400

    fab redirecting to home

--- app/src/main/java/com/example/pokemon_app/MainActivity.java
+++ app/src/main/java/com/example/pokemon_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pokemon_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -69,8 +70,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
